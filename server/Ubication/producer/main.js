186485cbae6ccac3564d247134b9518ac3918a6f
Diego Carrillo <diego.carrillo1@mail.udp.cl>
2022-10-28 00:24:34 -0300

    .
    .

--- server/Ubication/producer/main.js
+++ server/Ubication/producer/main.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const { Kafka } = require("kafkajs");
+const { json } = require("body-parser");
 
 //-------------------------------------------
 
@@ -33,14 +34,13 @@ var kafka = new Kafka({
 app.post("/ubication", (req, res) => {
   (async () => {
       const producer = kafka.producer();
-      //const admin = kafka.admin();
+
       await producer.connect();
       const { id,coordenadas , denuncia } = req.body;
-      //var time = Math.floor(new Date() / 1000);
       let ubication = {
         id: id,
-        coordenadas:coordenadas,
-        denuncia:denuncia 
+        coordenadas: coordenadas,
+        denuncia: denuncia 
       }
       value = JSON.stringify(ubication)
       if(ubication["denuncia"] == 1){
@@ -49,11 +49,11 @@ app.post("/ubication", (req, res) => {
          const CarroProfugo = [{
             topic: 'ubication',
             partition:1,
-            messages:[{value:JSON.stringify(ubication),partition: 1}]
+            messages:[{value:JSON.stringify(value),partition: 1}]
           },
         ]
         await producer.sendBatch({CarroProfugo})
-        console.log("Envie", ubication)
+        console.log("Envie", JSON.stringify(ubication))
       }
       else{
         console.log("Carrito Limpio.")
